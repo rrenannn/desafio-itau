05687b5e9c1d2aa7acbebe6aeb89459fe2fd511c
renan <renan.gamero1313@gmail.com>
2025-04-01 18:07:43 -0300

    package model created

---
+++ src/main/java/br/com/rrenannn/desafioitau/desafio_itau/transacao/model/TransacaoEntity.java
@@ -0,0 +1,10 @@
+package br.com.rrenannn.desafioitau.desafio_itau.transacao.model;
+
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class TransacaoEntity {
+    private BigDecimal valor;
+    private OffsetDateTime dataHora;
+}
---
+++ src/test/java/br/com/rrenannn/desafioitau/desafio_itau/DesafioItauApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rrenannn.desafioitau.desafio_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
